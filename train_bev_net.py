8bbe4f8bf3309094b10e0dd3b432d0fef53e0f3f
AlejandroZam <Alejoz0524@gmail.com>
2023-04-24 01:33:11 +0000

    i loaded swin backbone but it takes alot of memory and is slower to converge

--- train_bev_net.py
+++ train_bev_net.py
@@ -277,7 +277,7 @@ def setup(args):
     cfg.SOLVER.BASE_LR = 0.00020  # pick a good LR
     cfg.SOLVER.MAX_ITER = 1000 #5000    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
     cfg.SOLVER.STEPS = []        # do not decay learning rate
-    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128  # The "RoIHead batch size". 128 is faster, and good enough for this toy dataset (default: 512)
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 8 # was 128 # The "RoIHead batch size". 128 is faster, and good enough for this toy dataset (default: 512)
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 3  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)  
     cfg.freeze()
     default_setup(cfg, args)
